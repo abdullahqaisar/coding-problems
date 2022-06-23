f48ce6ec81c33b46d163e0b6901375b0f3a97a30
Abdullah Qaisar <abdullahqaisarr@gmail.com>
2022-06-23 23:25:57 +0500

    Basic Data Types > Find the Runner-Up Score!

---
+++ HackerRank/FindTheRunnerUpScore.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    n = int(input())
+    a = [int(x) for x in input().split()]
+    largest = secondlargest = -100
+    for x in a:
+        if x > largest:
+            tmp = largest
+            largest = x
+            secondlargest = tmp
+        elif x > secondlargest and x != largest:
+            secondlargest = x
+    print(secondlargest)
