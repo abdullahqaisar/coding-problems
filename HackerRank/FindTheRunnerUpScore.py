6308d7e9437a6b4255a48d033e8335577f9d9198
Abdullah Qaisar <abdullahqaisarr@gmail.com>
2022-06-25 16:07:46 +0500

    Solved using a list

--- HackerRank/FindTheRunnerUpScore.py
+++ HackerRank/FindTheRunnerUpScore.py
@@ -1,12 +1,13 @@
 if __name__ == '__main__':
     n = int(input())
-    a = [int(x) for x in input().split()]
-    largest = secondlargest = -100
-    for x in a:
-        if x > largest:
-            tmp = largest
-            largest = x
-            secondlargest = tmp
-        elif x > secondlargest and x != largest:
-            secondlargest = x
-    print(secondlargest)
+    arr = map(int, input().split())
+    
+    list = [-100, -100]
+    for x in arr:
+        if x > list[0]:
+            temp = list[0]
+            list[0] = x
+            list[1] = temp
+        elif x < list[0] and x > list[1]:
+            list[1] = x
+    print(list[1])
