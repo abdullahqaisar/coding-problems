964260a17a482e91a0a254c83b1f8846377c3b59
abdullahqaisar <abdullahqaisarr@gmail.com>
2022-10-29 21:55:12 +0500

    Showroom management system

---
+++ PythonPractice/showroomManagement.py
@@ -0,0 +1,56 @@
+##A vehicle showroom management system project!
+
+class Showroom:
+    showroom_name = ""
+    showroom_address = ""
+
+    def __init__():
+        pass
+    pass
+
+class Vehicle:
+    vehicle_name = ""
+    milage = ""
+    companyName = ""
+    model = ""
+    price = ""
+    
+    def __init(self, vn, m, cn, mod, p):
+        vehicle_name = vn
+        milage = m
+        companyName = cn
+        model = mod
+        price = p
+    
+    
+    pass
+
+class Big_Vehicle:
+    pass
+
+class Small_Vehicle:
+    pass
+
+class Bike:
+    pass
+
+class Car:
+    pass
+
+class Bus:
+    pass
+
+class Truck:
+    pass
+
+class Address:
+    pass
+
+class Account:
+    pass
+
+class Teachers:
+    pass
+
+if __name__ == "__main__":
+    print("Welcome to School Management System")
