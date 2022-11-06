8f57ab297f9a5096814d85276a8090b15935f014
abdullahqaisar <abdullahqaisarr@gmail.com>
2022-11-06 22:15:17 +0500

    Solved using old metho

---
+++ HackerRank/Array - DS.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'reverseArray' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def reverseArray(a):
+    x = len(a) - 1
+    y = []
+    for i in a:
+        y.append(a[x])
+        x = x - 1
+    return y
+               
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = reverseArray(arr)
+
+    fptr.write(' '.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
