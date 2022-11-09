f261cce62f529de2300a948ddd56f74d3d54454d
abdullahqaisar <abdullahqaisarr@gmail.com>
2022-11-09 14:50:37 +0500

    created classiness program

--- PythonPractice/LinkedList/main.py
+++ PythonPractice/LinkedList/main.py
@@ -3,12 +3,14 @@ from linked_list import Linked_List
 my_linked_list = Linked_List()
 
 my_linked_list.insert_node(2)
-
 my_linked_list.insert_node(3)
 my_linked_list.insert_node(4)
 my_linked_list.insert_node(10)
 my_linked_list.insert_node(7)
+
 my_linked_list.traverse()
+
+
 print(my_linked_list.delete_node(7))
 my_linked_list.traverse()
 
