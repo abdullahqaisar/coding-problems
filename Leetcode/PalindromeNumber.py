98930b183df66970774e6d2e727f3cb50f5b3e31
abdullahqaisar <abdullahqaisarr@gmail.com>
2022-12-25 22:16:32 +0500

    solved palindrome number

---
+++ Leetcode/PalindromeNumber.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPalindrome(self, x):
+        if x < 0:
+            return False
+
+        ranger = 1
+        while x / ranger >= 10:
+            ranger *= 10
+
+        while x:
+            left = x / ranger
+            right = x % 10
+            if left != right:
+                return False
+
+            x = (x % ranger) / 10
+            ranger /= 100
+
+        return True
