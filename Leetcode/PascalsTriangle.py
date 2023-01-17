46cda019d74f1680b0714775a1e90c100c9a70e1
abdullahqaisar <abdullahqaisarr@gmail.com>
2023-01-17 17:19:35 +0500

    solved pascals triangle

---
+++ Leetcode/PascalsTriangle.py
@@ -0,0 +1,14 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        result = []
+        for i in range(numRows):
+            arr = []
+            arr.append(1)
+
+            if i > 0:
+                for j in range(1, i):
+                    num = result[i-1][j-1] + result[i-1][j]
+                    arr.append(num)
+                arr.append(1)
+            result.append(arr)
+        return result
