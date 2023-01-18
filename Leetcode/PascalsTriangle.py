127afcf41cb0d8ee6cdb95e7e2589a32bede0a3e
abdullahqaisar <abdullahqaisarr@gmail.com>
2023-01-18 23:57:42 +0500

    changed pascals triangle

--- Leetcode/PascalsTriangle.py
+++ Leetcode/PascalsTriangle.py
@@ -4,7 +4,6 @@ class Solution:
         for i in range(numRows):
             arr = []
             arr.append(1)
-
             if i > 0:
                 for j in range(1, i):
                     num = result[i-1][j-1] + result[i-1][j]
