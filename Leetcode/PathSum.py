ba633c5babd58e132a5218da87e8ed3d863bfe7f
abdullahqaisar <abdullahqaisarr@gmail.com>
2023-04-28 20:43:32 +0500

    solved path sum using recursive dfs

---
+++ Leetcode/PathSum.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        
+        def sum(root, s):
+            if not root:
+                return False
+            s = s + root.val
+            if not root.left and not root.right and s == targetSum:
+                return True
+            return sum(root.left, s) or sum(root.right, s)
+        return sum(root, 0)
